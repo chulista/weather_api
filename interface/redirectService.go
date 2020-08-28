1a421f6d5aadcc1a5158ee909020bcf557158161
chulito <cartagena.al@gmail.com>
2020-08-28 11:46:27 -0300

    initiation

---
+++ interface/redirectService.go
@@ -0,0 +1,11 @@
+package _interface
+
+import (
+	"github.com/chulista/weather_api/domain"
+	//	"github.com/mercadolibre/fury_mpcs-preferences-api/internal/domain/model""
+)
+
+type RedirectService interface {
+	Find(code string) (*domain.Redirect, error)
+	Store(redirect *Redirect) error
+}

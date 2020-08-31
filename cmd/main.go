30e447c4c0eb06fe194449a0fcacd1763a24870a
chulito <cartagena.al@gmail.com>
2020-08-31 12:20:51 -0300

    change main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,80 +2,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/chulista/weather_api/application"
-	"github.com/chulista/weather_api/handlers"
-	mr "github.com/chulista/weather_api/infrastructure/persistence/mongodb"
-	rr "github.com/chulista/weather_api/infrastructure/persistence/redis"
-	"github.com/chulista/weather_api/interfaces"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"log"
+	"io"
 	"net/http"
-	"os"
-	"os/signal"
-	"strconv"
-	"syscall"
 )
 
-
-func main() {
-	repo := chooseRepo()
-	service := application.NewRedirectService(repo)
-	handler := handlers.NewHandler(service)
-
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	r.Get("/{code}", handler.Get)
-	r.Post("/", handler.Post)
-
-	errs := make(chan error, 2)
-	go func() {
-		fmt.Println("Listening on port :8000")
-		errs <- http.ListenAndServe(httpPort(), r)
-
-	}()
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT)
-		errs <- fmt.Errorf("%s", <-c)
-	}()
-
-	fmt.Printf("Terminated %s", <-errs)
-
+func main(){
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":80", nil)
 }
 
-func httpPort() string {
-	port := "8000"
-	if os.Getenv("PORT") != "" {
-		port = os.Getenv("PORT")
-	}
-	return fmt.Sprintf(":%s", port)
-}
-//choose persistent implementation
-func chooseRepo() interfaces.RedirectRepository {
-	switch os.Getenv("URL_DB") {
-	case "redis":
-		redisURL := os.Getenv("REDIS_URL")
-		repo, err := rr.NewRedisRepository(redisURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return repo
-	case "mongo":
-		mongoURL := os.Getenv("MONGO_URL")
-		mongodb := os.Getenv("MONGO_DB")
-		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
-		repo, err := mr.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return repo
-	}
-	return nil
+func index(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "hello from chulito")
 }

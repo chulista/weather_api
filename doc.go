6dbfd0c9fa2e60206ddf2b77abaf8b34389bd007
chulito <cartagena.al@gmail.com>
2020-12-01 16:35:02 -0300

    clean repository

--- doc.go
+++
@@ -1,4 +0,0 @@
-/*
-Package weather_api some extra documentation just to test the way I should do it
-*/
-package weather_api
--- interfaces/repository.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import "github.com/chulista/weather_api/domain"
-
-type RedirectRepository interface {
-	Find(code string) (*domain.Redirect, error)
-	Store(redirect *domain.Redirect) error
-}
